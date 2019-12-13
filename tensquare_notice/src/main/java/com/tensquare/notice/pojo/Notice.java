bbe49fb1f5024d50facf68457ae835a15ced5035
china-xiao <xiaoxh_1@163.com>
2019-12-13 10:07:51 +0800

    分支第一次提交

---
+++ tensquare_notice/src/main/java/com/tensquare/notice/pojo/Notice.java
@@ -0,0 +1,17 @@
+package com.tensquare.notice.pojo;
+
+import com.baomidou.mybatisplus.annotations.TableName;
+
+import java.io.Serializable;
+
+/**
+ * @Description： TODO
+ * @Author: xiaoxinghua
+ * @Date: Created in 2019/12/13 10:06
+ * @Version: 0.0.1
+ **/
+@TableName("tb_notice")
+public class Notice implements Serializable {
+
+
+}
