efc8b5996971fb1c50686e32ae28224dce27ac1c
china-xiao <xiaoxh_1@163.com>
2019-12-13 10:22:18 +0800

    master  创建NoticeFresh

---
+++ tensquare_notice/src/main/java/com/tensquare/notice/pojo/NoticeFresh.java
@@ -0,0 +1,15 @@
+package com.tensquare.notice.pojo;
+
+import com.baomidou.mybatisplus.annotations.TableName;
+
+/**
+ * @Description： TODO
+ * @Author: xiaoxinghua
+ * @Date: Created in 2019/12/13 10:20
+ * @Version: 0.0.1
+ **/
+@TableName("tb_notice_fresh")
+public class NoticeFresh {
+    private String userId;
+    private String noticeId;
+}
